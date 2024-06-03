bb992fc1420688bad9ff9ff4f047210b88a64a17
Saurabh Beniwal <saurabhbeniwal21062003@gmail.com>
2024-06-03 19:45:25 +0530

    Update server.js

--- server.js
+++ server.js
@@ -24,9 +24,9 @@ const app=express();
 
 //middlewares
 app.use(cors());
-app.use(express.json())
-app.use(morgan('dev'))
-app.use(express.static(path.join(__dirname, './client/build')))
+app.use(express.json());
+app.use(morgan('dev'));
+app.use(express.static(path.join(__dirname, './client/build')));
 
 //routes
 app.use("/api/v1/auth",authRoutes);
