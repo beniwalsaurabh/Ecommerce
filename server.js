40599ea5bf69b9bb151606095ce1c355f0c424dc
Saurabh Beniwal <saurabhbeniwal21062003@gmail.com>
2024-06-03 11:58:02 +0530

    Update server.js

--- server.js
+++ server.js
@@ -7,11 +7,17 @@ import authRoutes from './routes/authRoute.js'
 import categoryRoutes from './routes/categoryRoutes.js'
 import productRoutes from "./routes/productRoutes.js";
 import cors from 'cors';
-import path from 'path'
+import path from 'path';
+import {fileURLToPath} from 'url';
 
 dotenv.config();
 
+
 connectDB();
+
+//esmodule fix
+const __filename=fileURLToPath(import.meta.url);
+const __dirname=path.dirname(__filename);
 
 //rest object
 const app=express();
